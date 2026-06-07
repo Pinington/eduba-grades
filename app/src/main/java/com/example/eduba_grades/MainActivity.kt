19137480aa89026fbfe023f452cefbf349700010
Pinington <georges5757c@gmail.com>
2026-06-07 10:09:52 +0200

    Removed default greeting

--- app/src/main/java/com/example/eduba_grades/MainActivity.kt
+++ app/src/main/java/com/example/eduba_grades/MainActivity.kt
@@ -17,6 +17,7 @@ import androidx.compose.material3.CardColors
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -37,40 +38,19 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
-        val pv = PageViewer();
-
         setContent {
             EdubaGradesTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
                     Column {
                         BannerItem()
                         SpeView()
                     }
-
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    EdubaGradesTheme {
-        Greeting("Android")
     }
 }
 
