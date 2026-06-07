06aa1c3676204ac20836f3a4304b22e38e252bf2
Pinington <georges5757c@gmail.com>
2026-06-07 11:53:15 +0200

    Added backhandler, and spe/sem variables

--- app/src/main/java/com/example/eduba_grades/PageViewer.kt
+++ app/src/main/java/com/example/eduba_grades/PageViewer.kt
@@ -1,5 +1,6 @@
 package com.example.eduba_grades
 
+import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -11,6 +12,7 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -29,24 +31,60 @@ val listSpe : List<String> = listOf(
 
 enum class Screen {
     SPECIALITE,
-    SEMESTRE
+    SEMESTRE,
+    MATIERES
+}
+
+enum class Spe (val value : Int) {
+    // https://stackoverflow.com/questions/53523948/how-do-i-create-an-enum-from-an-int-in-kotlin
+    None(0),
+    ASE(1),
+    GI(2),
+    IR(3),
+    MECA(4),
+    TF(5);
+
+    companion object {
+        fun fromInt(value: Int) = entries.first { it.value == value }
+    }
 }
 
 @Composable
 fun App() {
     var currentScreen by remember { mutableStateOf(Screen.SPECIALITE) }
 
+    BackHandler(enabled = currentScreen != Screen.SPECIALITE) {
+        currentScreen = when (currentScreen) {
+            Screen.MATIERES -> Screen.SEMESTRE
+            Screen.SEMESTRE -> Screen.SPECIALITE
+            else -> Screen.SPECIALITE
+        }
+    }
+
+    var specialite by remember { mutableStateOf(Spe.None) }
+    var semestre by remember {mutableIntStateOf(0)}
+
     when (currentScreen) {
         Screen.SPECIALITE -> {
             SpeView(
-                onSpecialityClick = {
+                onSpecialityClick = { spe ->
                     currentScreen = Screen.SEMESTRE
+                    specialite = Spe.fromInt(spe)
                 }
             )
         }
 
         Screen.SEMESTRE -> {
-            SemView()
+            SemView(
+                onSemesterClick = { sem ->
+                    currentScreen = Screen.MATIERES
+                    semestre = sem
+                }
+            )
+        }
+
+        Screen.MATIERES -> {
+
         }
     }
 }
@@ -78,7 +116,7 @@ fun SpeView(spe: List<String> = listSpe, onSpecialityClick: (Int) -> Unit) {
 }
 
 @Composable
-fun SemView(sem: Int = 6) {
+fun SemView(sem: Int = 6, onSemesterClick: (Int) -> Unit) {
     LazyColumn (
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier
@@ -106,6 +144,9 @@ fun SemView(sem: Int = 6) {
                 modifier = Modifier
                     .padding(6.dp)
                     .fillMaxWidth(0.95F)
+                    .clickable{
+                        onSemesterClick(it + 1)
+                    }
             ) {
                 Text (
                     text = "Semestre ${it + 1}",
