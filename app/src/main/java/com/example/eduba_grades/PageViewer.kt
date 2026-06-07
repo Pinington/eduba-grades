36520330419c33c7f9fcfc1deb1d56d4236e1229
Pinington <georges5757c@gmail.com>
2026-06-07 11:35:56 +0200

    Made semester view

--- app/src/main/java/com/example/eduba_grades/PageViewer.kt
+++ app/src/main/java/com/example/eduba_grades/PageViewer.kt
@@ -1,15 +1,23 @@
 package com.example.eduba_grades
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 val listSpe : List<String> = listOf(
     "Automatique & Systemes Embarques",
@@ -19,29 +27,50 @@ val listSpe : List<String> = listOf(
     "Textiles et Fibres"
 )
 
-class PageViewer (
-    val specialite: Int, // 0: ASE, 1: GI, 2: IR, 3: MECA, 4: TF
-    val semestre: Int
-)
+enum class Screen {
+    SPECIALITE,
+    SEMESTRE
+}
 
 @Composable
-fun SpeView(spe: List<String> = listSpe) {
+fun App() {
+    var currentScreen by remember { mutableStateOf(Screen.SPECIALITE) }
+
+    when (currentScreen) {
+        Screen.SPECIALITE -> {
+            SpeView(
+                onSpecialityClick = {
+                    currentScreen = Screen.SEMESTRE
+                }
+            )
+        }
+
+        Screen.SEMESTRE -> {
+            SemView()
+        }
+    }
+}
+
+@Composable
+fun SpeView(spe: List<String> = listSpe, onSpecialityClick: (Int) -> Unit) {
     LazyColumn (
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier
             .padding(0.dp, 18.dp)
             .fillMaxWidth()
     ) {
-        items(spe) {
-            Card (
+        itemsIndexed(spe) { index, item ->
+            Card(
                 modifier = Modifier
                     .padding(6.dp)
-                    .fillMaxWidth(0.95F)
+                    .fillMaxWidth(0.95f)
+                    .clickable {
+                        onSpecialityClick(index)
+                    }
             ) {
-                Text (
-                    text = it,
-                    modifier = Modifier
-                        .padding(12.dp)
+                Text(
+                    text = item,
+                    modifier = Modifier.padding(12.dp)
                 )
             }
         }
@@ -50,12 +79,33 @@ fun SpeView(spe: List<String> = listSpe) {
 
 @Composable
 fun SemView(sem: Int = 6) {
-    LazyColumn {
+    LazyColumn (
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxWidth()
+    ) {
         items(sem) {
+
+            if (it % 2 == 0) {
+                Box (
+                    modifier = Modifier
+                        .fillMaxWidth(0.95F)
+                        .padding(top = 24.dp)
+                ) {
+                    Text (
+                        text = "${(it / 2) + 1}A",
+                        fontSize = 12.sp,
+                        modifier = Modifier
+                            .padding(start = 12.dp)
+                            .align(Alignment.CenterStart)
+                    )
+                }
+            }
+
             Card (
                 modifier = Modifier
                     .padding(6.dp)
-                    .fillMaxWidth()
+                    .fillMaxWidth(0.95F)
             ) {
                 Text (
                     text = "Semestre ${it + 1}",
