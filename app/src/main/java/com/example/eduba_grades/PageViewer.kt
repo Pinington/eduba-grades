5491bd896828e7d1546eadc840e9dfc788d68a45
Pinington <georges5757c@gmail.com>
2026-06-07 21:19:23 +0200

    Added grade input

--- app/src/main/java/com/example/eduba_grades/PageViewer.kt
+++ app/src/main/java/com/example/eduba_grades/PageViewer.kt
@@ -2,15 +2,25 @@ package com.example.eduba_grades
 
 import android.util.Log
 import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
@@ -21,6 +31,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -190,24 +203,67 @@ fun SemSelectView(sem: Int = 6, onSemesterClick: (Int) -> Unit) {
 
 @Composable
 fun DisplaySubjects(unites : List<UniteEnseignement>) {
-    LazyColumn {
+    LazyColumn (
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(bottom = 60.dp)
+    ) {
         items(unites) { ue ->
 
-            Text(
-                text = ue.name,
-                fontSize = 24.sp,
-                color = Color.DarkGray,
-                modifier = Modifier.padding(start = 12.dp, top = 16.dp)
-            )
-
-            ue.modules.forEach { subject ->
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 30.dp)
+            ) {
                 Text(
-                    text = subject.name,
-                    fontSize = 20.sp,
+                    text = ue.name,
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Bold,
                     color = Color.White,
-                    modifier = Modifier.padding(start = 16.dp, top = 8.dp)
+                    modifier = Modifier
+                        .padding(10.dp, 5.dp)
+                        .fillMaxWidth()
+                        .height(40.dp)
                 )
             }
+
+
+            ue.modules.forEachIndexed { index, subject ->
+                val color = if (index % 2 == 0) Color.Gray else Color.DarkGray
+                Row (
+                    horizontalArrangement = Arrangement.spacedBy(20.dp),
+                    modifier = Modifier
+                        .fillMaxWidth(0.95F)
+                        .background(color)
+                        .padding(6.dp)
+                ) {
+                    Text(
+                        text = subject.name,
+                        fontSize = 20.sp,
+                        color = Color.White
+                    )
+
+                    Spacer(modifier = Modifier.weight(1f))
+
+                    var searchQuery by remember {mutableStateOf("")}
+                    TextField(
+                        value = searchQuery,
+                        onValueChange = {
+                            searchQuery = it
+                        },
+                        singleLine = true,
+                        modifier = Modifier
+                            .padding()
+                            .height(28.dp)
+                            .width(40.dp)
+                    )
+                }
+            }
+        }
+
+        item {
+            Spacer(modifier = Modifier.height(80.dp))
         }
     }
 }
